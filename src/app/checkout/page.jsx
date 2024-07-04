4323eddf6e414cd65bed873d6391eb756ee8b45f
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-07-04 20:52:22 +0300

    fix problem of payment via stripe

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,32 +1,28 @@
 "use client"
-import React, { useEffect, useState } from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from './CheckoutForm';
-import { useRouter } from 'next/navigation'
-
+import React from 'react';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
+import CheckoutForm from "./CheckoutForm";
+import { useSearchParams } from 'next/navigation'
 
 const Checkout = () => {
-  const [totalAmount, setTotalAmount] = useState(0);
-  const router = useRouter(); // Replace with `next/router` if using Next.js 12+
+ const searchParams = useSearchParams()
+ 
+  const searchParam = searchParams.get('totalAmount')
+    let totalAmount=Number(searchParam)
+      const options = {
+          mode:'payment',
+          currency:'usd',
+          amount:+`${+totalAmount*100}`,
 
-  useEffect(() => {
-    const { totalAmount } = router.query; // Fetch query parameter 'totalAmount'
-    if (totalAmount) {
-      setTotalAmount(Number(totalAmount));
-    }
-  }, [router.query]);
-
-  const options = {
-    currency: 'usd',
-    amount: totalAmount * 100, // Stripe expects amount in cents
   };
-
   return (
-    <Elements stripe={stripePromise}>
-      <CheckoutForm totalAmount={totalAmount} options={options} />
-    </Elements>
+  <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm totalAmount={totalAmount}/>
+    </Elements>      
+
+ 
   );
 };
 
